c0498f4882a4414ba943c10a6632d62a9b4f8bce
Douglas Tarr <douglas.tarr@gmail.com>
2013-04-19 10:10:39 -0700

    some ko libs

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ knockout
 ========
 
 My Knockout Libraries
+
+A list of my knockout libraries
+
+* ScrollToView - automatically scrolls to the changed element in an observable array for overflowing divs
+* ExecuteOnEnter - lets you use the enter key to submit a change
+
+
---
+++ ScrollToView.js
@@ -0,0 +1,20 @@
+﻿ko.bindingHandlers.ScrollToView = {
+	init: function (element, valueAccessor) {
+		// Initially set the element to be instantly visible/hidden depending on the value
+		//var value = valueAccessor();
+		//$(element).hide();
+		//scrollToEntry(element, valueAccessor);
+	},
+	update: function (element, valueAccessor, allBindingsAccessor, viewModel, bindingContext) {
+		// Whenever the value subsequently changes, slowly fade the element in or out
+		//var value = valueAccessor();
+		var item = ko.utils.unwrapObservable(valueAccessor());
+		var entry = $(element);
+		// find the entry in the array that changed
+		var scrollable = entry.parent();
+		var scrollTop = scrollable.scrollTop() + entry.offset().top - scrollable.offset().top;
+		scrollable.scrollTop(scrollTop);
+	}
+
+};
+
